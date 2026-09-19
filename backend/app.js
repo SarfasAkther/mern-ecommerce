874754e1c659a99e41dcf6e31b08ec389ec4f5fa
Sarfas Akther <sarfasakthwe123@gmail.com>
2026-09-19 11:37:41 +0530

    Add production start script

--- backend/app.js
+++ backend/app.js
@@ -32,6 +32,9 @@ app.use("/api/payment", paymentRoutes);
 app.use("/api/wishlist", wishlistRoutes);
 app.use("/api/admin", adminRoutes);
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
